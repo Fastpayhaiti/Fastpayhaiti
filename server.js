6dd50fe38e538641dd07f626fec065c92462b12d
Fastpayhaiti <fastpayhaiti@gmail.com>
2026-03-24 05:40:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const { Pool } = require("pg");
-
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 
 
